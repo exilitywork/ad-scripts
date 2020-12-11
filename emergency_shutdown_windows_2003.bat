56d228b9cba67b5b4977d96d836278f6abb1c5b7
Капешко Олег А <oleg.kapeshko@belwest.com>
2020-12-11 14:11:47 +0300

    Upload New File emergency shutdown windows 2003.bat

---
+++ emergency_shutdown_windows_2003.bat
@@ -0,0 +1,12 @@
+Are you shure you want to shutdown all Windows 2003?
+timeout /t 20 /nobreak
+Sending commands
+shutdown /s /m \\belw-appsvr /t 003
+shutdown /s /m \\belw /t 003
+shutdown /s /m \\belw1 /t 003
+shutdown /s /m \\win2003_1c /t 003
+shutdown /s /m \\belw-artist /t 003
+Complete
+timeout /t 10 /nobreak
+Have a nice day
+close
